59ba0e1933d673d2c20d0e90287a558990767d0d
ALBAS23 <maxbouxmasa3939@gmail.com>
2020-11-30 10:39:10 +0900

    /config/application.rbの修正

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,6 @@ module ChatApp
       g.helper false
       g.test_framework false
     end
-  end
-  
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
